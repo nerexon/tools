f3a37ff6e034517fe2576874ace057c0b938caaf
NerexonAdmin <166040621+NerexonAdmin@users.noreply.github.com>
2024-05-16 20:59:58 +0000

    First commit ever

--- scripts/numbergenerator.js
+++ scripts/numbergenerator.js
@@ -1,15 +1,21 @@
 let submitButton = document.getElementById("submit")
-submitButton.onclick = () => {
+
+submitButton.onclick = (event) => {
+  event.preventDefault();
+  
   let min = document.getElementById("min").value
   let max = document.getElementById("max").value
   let amount = document.getElementById("amount").value
   let text = document.getElementById("text")
-  text.value = ""
-  if(amount > 99999){
-    return alert("You exceeded the maximum amount of numbers to generate. Please try again. The limit is 99999.")
+  let baseText = ""
+  
+  if(amount > 150){
+    return alert("You exceeded the maximum amount of numbers to generate. Please try again. The limit is 150.")
   }
   for(let i = 0; i < amount; i++){
-    let number = Math.floor(Math.random() * (max - min)) + parseInt(min)
-    text.value += `${number}\n`
+    let number = Math.floor(Math.random() * (max - min + 1)) + parseInt(min)
+    baseText += `${number}, `
   }
+  text.style.display = 'block';
+  text.innerHTML = baseText.slice(0, -2)
 }
